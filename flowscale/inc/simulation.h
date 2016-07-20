652da9cd90aa2b3e3bf2e608ccd9eb5c7bc278c9
Sigurgeir <sigurgeirg@gmail.com>
2016-07-20 17:22:26 +0000

    QCustomPlot folder added to test (has to be with these 3 capital letters ..)

--- flowscale/inc/simulation.h
+++ flowscale/inc/simulation.h
@@ -1,5 +1,35 @@
 #ifndef SIMULATION
 #define SIMULATION
 
+#include <QThread>
+#include <inttypes.h>
+#include <iostream>
+#include <math.h>
+#include <QDebug>
+
+class Simulation : public QThread
+{
+    Q_OBJECT
+public:
+    explicit Simulation(QObject *parent = 0);
+    ~Simulation();
+
+    void run();
+    int *statusRegisterBinary(uint16_t number[]);
+
+    int *statusRegisterBinaryTempValue;
+    int *statusRegisterBinaryReturnValue;
+    uint16_t data[10];      // Incoming data values are stored for a moment in a vector of size 10
+
+    bool modbusConnected;   // Flag that allows while loop to scan through incoming data
+
+
+signals:
+    void receivedWeight(int);
+
+public slots:
+
+};
+
 #endif // SIMULATION
 
